8d315415daaf3e54324c50d0b8cbe04e4dd2cc67
hrishi82 <53048695+hrishi82@users.noreply.github.com>
2022-11-10 19:06:18 +0530

    css refactor and added a how-to-use note

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,9 @@
 import { Search } from "../components/search/Search";
-
+import "./homepage.css"
 export const HomePage = () => {
   return (
-    <div>
+    <div className="homepage-container">
+      <p className="homepage-infotext">Search for words like: "john" or numbers like 1</p>
       <Search />
     </div>
   );
